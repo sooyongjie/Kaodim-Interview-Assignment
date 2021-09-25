b47cc58b7ca983de92ac80ca3b11a06aa63c7e07
sooyongjie <sooyongjie@gmail.com>
2021-09-25 17:08:01 +0800

    Buttons are disabled if not fulfilled requirements

--- public/script.js
+++ public/script.js
@@ -6,14 +6,14 @@ const form = {
       question_type: "TextQuestion",
       prompt: "What is your first answer?",
       is_required: false,
-      min_char_length: 15,
+      min_char_length: 2,
     },
     {
       id: 2448,
       question_type: "TextQuestion",
       prompt: "What is your second answer?",
       is_required: true,
-      min_char_length: 100,
+      min_char_length: 2,
     },
     {
       id: 2500,
@@ -30,28 +30,73 @@ const nextBtn = document.querySelector("#next-btn");
 const formTitle = document.querySelector("#form-title");
 const formQues = document.querySelector("#form-question");
 const formInput = document.querySelector("#form-input");
+const requirementTxt = document.querySelector("#requirements")
 const quesNum = document.querySelector("#question-num");
 const element = document.querySelector("#elID");
-const userInput = ['','',''];
+const userInput = ['', '', ''];
+
+
 let currQues = 0;
-let requiredInput = false;
 let numOfQues;
+let charLen = formInput.value.length;
+let min_char_length = 0;
+let is_required = false;
+
 
 window.onload = () => {
   loadQuestions(form.questions[currQues]);
   numOfQues = form.questions.length - 1;
 };
 
+const enableButton = (x) => {
+  x ? nextBtn.disabled = false : nextBtn.disabled = true
+}
+
+const checkCharLength = () => {
+  if (!is_required && charLen == 0) {
+    enableButton(true)
+  }
+  else if (!is_required && charLen >= min_char_length) {
+    enableButton(true)
+  }
+  else if (is_required && charLen < min_char_length) {
+    enableButton(false)
+  }
+  else if (is_required && charLen >= min_char_length) {
+    enableButton(true)
+  }
+  else {
+    enableButton(false)
+  }
+}
+
+formInput.oninput = () => {
+  charLen = formInput.value.length;
+  checkCharLength()
+};
+
+const checkInput = () => {
+  charLen = formInput.value.length;
+  checkCharLength()
+}
+
+
 const loadQuestions = (arr) => {
-  const { id, question_type, prompt, is_required, min_char_length } = arr;
+  let { id, question_type, prompt } = arr;
+  min_char_length = arr.min_char_length
+  is_required = arr.is_required
+
   formInput.value = "";
   formQues.textContent = prompt;
   quesNum.textContent = `${currQues + 1}. `;
   formInput.value = userInput[currQues]
+  is_required == true ? requirementTxt.textContent = "Field is required" : requirementTxt.textContent = "Optional";
+  min_char_length > 1 ? requirementTxt.textContent += `, ${min_char_length} characters are required.` : requirementTxt.textContent += `, ${min_char_length} character is required.`;
+  checkInput()
 };
 
 nextBtn.onclick = () => {
-  if (!requiredInput && currQues < numOfQues) {
+  if (currQues < numOfQues) {
     userInput[currQues] = formInput.value;
     ++currQues;
     loadQuestions(form.questions[currQues]);
